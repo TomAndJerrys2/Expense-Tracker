2177811850457b4ea2336e289845cb6e743362b5
kylev <kylev2005@hotmail.com>
2025-03-30 22:13:11 +0100

    backend

---
+++ proj-tracker/src/main/java/io/project_tracker/proj_tracker/ProjTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.project_tracker.proj_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjTrackerApplication.class, args);
+	}
+
+}
---
+++ proj-tracker/src/test/java/io/project_tracker/proj_tracker/ProjTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project_tracker.proj_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
